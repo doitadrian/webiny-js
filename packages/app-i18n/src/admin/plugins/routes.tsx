8d4470f38b415fd506d6bc7860d3ad5834659b55
Adrian Smijulj <adrian1358@gmail.com>
2020-04-03 12:58:52 +0200

    fix: add route for the texts module

--- packages/app-i18n/src/admin/plugins/routes.tsx
+++ packages/app-i18n/src/admin/plugins/routes.tsx
@@ -3,26 +3,47 @@ import Helmet from "react-helmet";
 import { Route } from "@webiny/react-router";
 import { AdminLayout } from "@webiny/app-admin/components/AdminLayout";
 import { I18NLocales } from "@webiny/app-i18n/admin/views/I18NLocales";
+import { I18NTexts } from "@webiny/app-i18n/admin/views/I18NTexts";
 import { SecureRoute } from "@webiny/app-security/components";
 import { RoutePlugin } from "@webiny/app/types";
 
-const plugin: RoutePlugin = {
-    name: "route-i18n-locales",
-    type: "route",
-    route: (
-        <Route
-            exact
-            path={"/i18n/locales"}
-            render={() => (
-                <SecureRoute roles={["i18n-locales"]}>
-                    <AdminLayout>
-                        <Helmet title={"I18N - Locales"} />
-                        <I18NLocales />
-                    </AdminLayout>
-                </SecureRoute>
-            )}
-        />
-    )
-};
+const plugin: RoutePlugin[] = [
+    {
+        name: "route-i18n-locales",
+        type: "route",
+        route: (
+            <Route
+                exact
+                path={"/i18n/locales"}
+                render={() => (
+                    <SecureRoute roles={["i18n-locales"]}>
+                        <AdminLayout>
+                            <Helmet title={"I18N - Locales"} />
+                            <I18NLocales />
+                        </AdminLayout>
+                    </SecureRoute>
+                )}
+            />
+        )
+    },
+    {
+        name: "route-i18n-texts",
+        type: "route",
+        route: (
+            <Route
+                exact
+                path={"/i18n/texts"}
+                render={() => (
+                    <SecureRoute roles={["i18n-texts"]}>
+                        <AdminLayout>
+                            <Helmet title={"I18N - Texts"} />
+                            <I18NTexts />
+                        </AdminLayout>
+                    </SecureRoute>
+                )}
+            />
+        )
+    }
+];
 
 export default plugin;
