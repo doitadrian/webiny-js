8188700dfb8c41881ce0b329b0477f4ef0f6eea6
Adrian Smijulj <adrian1358@gmail.com>
2020-04-03 13:14:16 +0200

    wip: wrap text into translation function

--- examples/apps/admin/src/App.js
+++ examples/apps/admin/src/App.js
@@ -7,35 +7,44 @@ import { AppInstaller } from "@webiny/app-admin/components/Install/AppInstaller"
 import { PageBuilderProvider } from "@webiny/app-page-builder/contexts/PageBuilder";
 import { SecurityProvider } from "@webiny/app-security/contexts/Security";
 import { I18NProvider } from "@webiny/app-i18n/contexts/I18N";
+import { I18NProvider as I18NReactProvider } from "@webiny/i18n-react/contexts/I18N"; // TODO @i18n: think of better naming
 import { CircularProgress } from "@webiny/ui/Progress";
-import "./App.scss";
-import plugins from "./plugins";
+import { i18n } from "@webiny/i18n";
 
+import plugins from "./plugins";
+import "./App.scss";
+
+const t = i18n.ns("apps/admin");
 registerPlugins(plugins);
 
 // Execute `init` plugins, they may register more plugins dynamically
 getPlugins("webiny-init").forEach(plugin => plugin.init());
 
 const securityProvider = (
-    <SecurityProvider loader={<CircularProgress label={"Checking user..."} />} />
+    <SecurityProvider loader={<CircularProgress label={t`Checking user...`} />} />
 );
 
 const App = () => {
+    // TODO @i18n: Think about the order of providers and props, then finish.
     return (
         <UiProvider>
-            <I18NProvider loader={<CircularProgress label={"Loading locales..."} />}>
-                <AppInstaller security={securityProvider}>
-                    <PageBuilderProvider>
-                        <ThemeProvider>
-                            {getPlugins("route").map(pl =>
-                                React.cloneElement(pl.route, {
-                                    key: pl.name,
-                                    exact: true
-                                })
-                            )}
-                        </ThemeProvider>
-                    </PageBuilderProvider>
-                </AppInstaller>
+            {/* Gets current locale and sends it to the "@webiny/i18n-react" provider */}
+            <I18NProvider loader={<CircularProgress label={t`Loading locales...`} />}>
+                {/* Send current locale and translations. */}
+                <I18NReactProvider>
+                    <AppInstaller security={securityProvider}>
+                        <PageBuilderProvider>
+                            <ThemeProvider>
+                                {getPlugins("route").map(pl =>
+                                    React.cloneElement(pl.route, {
+                                        key: pl.name,
+                                        exact: true
+                                    })
+                                )}
+                            </ThemeProvider>
+                        </PageBuilderProvider>
+                    </AppInstaller>
+                </I18NReactProvider>
             </I18NProvider>
         </UiProvider>
     );
